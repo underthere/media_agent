cc9536e7f1700ffaf79ba96a33d1c0496fbb4f95
underthere <cltobecl@outlook.com>
2023-08-14 21:43:11 +0800

    signal/slot impl

--- media_agent/include/signal.hpp
+++ media_agent/include/signal.hpp
@@ -16,11 +16,9 @@
 namespace asignal {
 
 namespace detail {
-template <typename Callback>
-struct slot_impl;
+template <typename Callback> struct slot_impl;
 
-template <typename Callback>
-struct signal_impl : nocopy {
+template <typename Callback> struct signal_impl : nocopy {
   using slot_vec = std::vector<std::weak_ptr<slot_impl<Callback>>>;
 
   signal_impl() : slots_(new slot_vec) {}
@@ -34,7 +32,7 @@ struct signal_impl : nocopy {
   auto clean() {
     std::lock_guard<std::mutex> lock(mutex_);
     cow();
-    slot_vec& list(*slots_);
+    slot_vec &list(*slots_);
     auto it = list.begin();
     while (it != list.end()) {
       if (it->expired()) {
@@ -49,15 +47,20 @@ struct signal_impl : nocopy {
   std::shared_ptr<slot_vec> slots_;
 };
 
-template <typename Callback>
-struct slot_impl : nocopy {
+template <typename Callback> struct slot_impl : nocopy {
   using Data = signal_impl<Callback>;
 
-  slot_impl(const std::shared_ptr<Data>& data, Callback&& callback) : data_(data), cb_(std::move(callback)), tie_(), tied_(false) {}
+  slot_impl(const std::shared_ptr<Data> &data, Callback &&callback)
+      : data_(data), cb_(std::move(callback)), tie_(), tied_(false) {}
+
+  slot_impl(const std::shared_ptr<Data> &data, Callback &&callback,
+            const std::shared_ptr<void> &tie)
+      : data_(data), cb_(std::move(callback)), tie_(tie), tied_(true) {}
 
   ~slot_impl() {
     std::shared_ptr<Data> data(data_.lock());
-    if (data) data->clean();
+    if (data)
+      data->clean();
   }
 
   std::weak_ptr<Data> data_;
@@ -65,51 +68,65 @@ struct slot_impl : nocopy {
   std::weak_ptr<void> tie_;
   bool tied_;
 };
-}  // namespace detail
+} // namespace detail
 
 using slot = std::shared_ptr<void>;
 
-template <typename Signature>
-class signal;
+template <typename Signature> class signal;
 
-template <typename R, typename... Args>
-class signal<R(Args...)> : nocopy {
- public:
+template <typename R, typename... Args> class signal<R(Args...)> : nocopy {
+public:
   using Callback = std::function<R(Args...)>;
-  using slot_type = slot_impl<Callback>;
-  using signal_type = ;
+  using slot_type = detail::slot_impl<Callback>;
+  using signal_type = detail::signal_impl<Callback>;
 
-  signal() : data_(new Data) {}
+  signal() : signal_(new signal_type) {}
+  ~signal() {}
 
-  auto connect(Callback&& callback) -> slot {
-    std::lock_guard<std::mutex> lock(data_->mutex_);
-    data_->cow();
-    auto slot = std::make_shared<slot_type>(data_, std::move(callback));
-    data_->slots_->push_back(slot);
+  auto connect(Callback &&func) -> slot {
+    std::shared_ptr<slot_type> slot(new slot_type(signal_, std::move(func)));
+    add(slot);
     return slot;
   }
 
-  auto operator()(Args... args) -> void {
-    std::lock_guard<std::mutex> lock(data_->mutex_);
-    data_->cow();
-    slot_type* slot = nullptr;
-    for (auto& weak_slot : *data_->slots_) {
-      if (auto strong_slot = weak_slot.lock()) {
-        if (slot) {
-          slot->cb_(args...);
+  auto call(Args &&...args) {
+    signal_type &signal(*signal_);
+    std::shared_ptr<typename signal_type::slot_vec> slots;
+    {
+      std::lock_guard<std::mutex> lock(signal.mutex_);
+      slots = signal.slots_;
+    }
+    typename signal_type::slot_vec &list(*slots);
+    for (typename signal_type::slot_vec::const_iterator it = list.begin();
+         it != list.end(); ++it) {
+      std::shared_ptr<slot_type> slot(it->lock());
+      if (slot) {
+        std::shared_ptr<void> guard;
+        if (slot->tied_) {
+          guard = slot->tie_.lock();
+          if (guard) {
+            slot->cb_(std::forward<Args>(args)...);
+          }
         } else {
-          slot = strong_slot.get();
-          slot->cb_(args...);
+          slot->cb_(std::forward<Args>(args)...);
         }
       }
     }
   }
 
- private:
-  using Data = signal_impl<Callback>;
-  std::shared_ptr<Data> data_;
+private:
+  auto add(const std::shared_ptr<slot_type> &slot) {
+    signal_type &signal(*signal_);
+    {
+      std::lock_guard<std::mutex> lock(signal.mutex_);
+      signal.cow();
+      signal.slots_->push_back(slot);
+    }
+  }
+
+  const std::shared_ptr<signal_type> signal_;
 };
 
-}  // namespace asignal
+} // namespace asignal
 
-#endif  // MEDIA_AGENT_SIGNAL_HPP
+#endif // MEDIA_AGENT_SIGNAL_HPP
